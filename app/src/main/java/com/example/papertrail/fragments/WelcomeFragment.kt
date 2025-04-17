c359123f0433d52fa01a6c40c89400fcd2442505
oliviatroitino <olivia@cygnuskraft.es>
2025-04-17 19:48:18 +0200

    Create WelcomeFragment

---
+++ app/src/main/java/com/example/papertrail/fragments/WelcomeFragment.kt
@@ -0,0 +1,32 @@
+package com.example.papertrail.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.papertrail.R
+import com.example.papertrail.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentWelcomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.loginButton.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_welcomeFragment_to_logInFragment)
+        }
+    }
+
+}
